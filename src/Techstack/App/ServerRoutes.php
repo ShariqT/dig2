7762a6d5066f931a3e116ddd5dde6b4c157f494e
Shariq Torres <shariq.torres@gmail.com>
2016-07-28 13:31:24 -0400

    added info from file uploads

--- src/Techstack/App/ServerRoutes.php
+++ src/Techstack/App/ServerRoutes.php
@@ -2,12 +2,28 @@
 
 
 $app->get('/', function() use ($app){
+
 	$app->render('index.html');
 });
 
-$app->get("/hello/:name", function() use ($app){
-	$app->render('private.html');
+
+$app->get("/details/:id", function($id) use ($app){
+	if($id == 1){
+		$data = array("hash" => "QmPL9M1anAGkgGrc8s613m6Dyhfosk3DSwR6FMRzWS33aj");
+		$data['title'] = "Warp Drive is Possible";
+		$data['name'] = "Prof. Blastoff";
+		$data['subject'] = "space exploration";
+		$data['school'] = "VCU";
+	}else{
+		$data = array("hash" => "QmbZf4VJkkCTfjmFYfxKRHDB8rQ9L4JS86sG7Wnw5a74bC");
+		$data['title'] = "Science Fiction inspires real-life science";
+		$data['name'] = "Dr. Kilgore Trout";
+		$data['subject'] = "space exploration";
+		$data['school'] = "University of Richmond";
+	}
+	$app->render("details.html", $data);
 });
+
 
 
 

fba361aff1f51980411e5c2f34f3306d55c2bf5f
Shariq Torres <shariq.torres@gmail.com>
2016-07-28 13:40:47 -0400

    added details and process info

--- src/Techstack/App/ServerRoutes.php
+++ src/Techstack/App/ServerRoutes.php
@@ -7,15 +7,21 @@ $app->get('/', function() use ($app){
 });
 
 
+$app->post("/process", function() use ($app){
+	$id = rand(0, 5);
+	$app->redirect("/details/" . $id);
+});
+
+
 $app->get("/details/:id", function($id) use ($app){
 	if($id == 1){
-		$data = array("hash" => "QmPL9M1anAGkgGrc8s613m6Dyhfosk3DSwR6FMRzWS33aj");
+		$data = array("hash" => "https://files.complictor.com/files/QmPL9M1anAGkgGrc8s613m6Dyhfosk3DSwR6FMRzWS33aj/");
 		$data['title'] = "Warp Drive is Possible";
 		$data['name'] = "Prof. Blastoff";
 		$data['subject'] = "space exploration";
 		$data['school'] = "VCU";
 	}else{
-		$data = array("hash" => "QmbZf4VJkkCTfjmFYfxKRHDB8rQ9L4JS86sG7Wnw5a74bC");
+		$data = array("hash" => "https://files.complictor.com/files/QmbZf4VJkkCTfjmFYfxKRHDB8rQ9L4JS86sG7Wnw5a74bC/");
 		$data['title'] = "Science Fiction inspires real-life science";
 		$data['name'] = "Dr. Kilgore Trout";
 		$data['subject'] = "space exploration";
